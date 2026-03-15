677886ca155b21ce50a9aad64ab850a326771140
Annisa Azzahra Putri <annisaazzahraputrizahra@gmail.com>
2026-03-15 16:11:00 +0700

    first commit

---
+++ TP_MODUL4_103022400033/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    private Dictionary<string, string> kodePosTable = new Dictionary<string, string>()
+    {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"}
+    };
+
+    public string getKodePos(string kelurahan)
+    {
+        return kodePosTable[kelurahan];
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        KodePos kp = new KodePos();
+        Console.WriteLine("Kode Pos Batununggal: " + kp.getKodePos("Batununggal"));
+    }
+}
